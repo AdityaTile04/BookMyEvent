66fd8f9752c1b4923da5e2fde628a03d6894cdf5
Aditya Tile <adityatile99@gmail.com>
2025-11-19 12:22:35 +0530

    feat: add EventController and global exception handling with ErrorDto

---
+++ backend/src/main/java/com/project/backend/controllers/EventController.java
@@ -0,0 +1,41 @@
+package com.project.backend.controllers;
+
+import com.project.backend.domain.CreateEventRequest;
+import com.project.backend.domain.dtos.CreateEventRequestDto;
+import com.project.backend.domain.dtos.CreateEventResponseDto;
+import com.project.backend.domain.entities.Event;
+import com.project.backend.mappers.EventMapper;
+import com.project.backend.services.EventService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping(params = "/api/v1/events")
+@RequiredArgsConstructor
+public class EventController {
+
+    private final EventMapper eventMapper;
+    private final EventService eventService;
+
+    @PostMapping
+    public ResponseEntity<CreateEventResponseDto> createEvent(
+            @AuthenticationPrincipal Jwt jwt,
+           @Valid @RequestBody CreateEventRequestDto createEventRequestDto) {
+                CreateEventRequest createEventRequest = eventMapper.fromDto( createEventRequestDto);
+        UUID userId = UUID.fromString(jwt.getSubject());
+        Event createEvent = eventService.createEvent( userId,  createEventRequest);
+        CreateEventResponseDto createEventResponseDto = eventMapper.toDto( createEvent );
+        return new ResponseEntity<>( createEventResponseDto, HttpStatus.CREATED );
+    }
+
+}
