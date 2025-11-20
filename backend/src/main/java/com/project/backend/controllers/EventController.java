6d025d79ae7787ee07afdef09f33c28e839d9be8
Aditya Tile <adityatile99@gmail.com>
2025-11-20 23:56:23 +0530

    fix: fix issue in EventController

--- backend/src/main/java/com/project/backend/controllers/EventController.java
+++ backend/src/main/java/com/project/backend/controllers/EventController.java
@@ -12,16 +12,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @RestController
-@RequestMapping(params = "/api/v1/events")
+@RequestMapping(path = "/api/v1/events")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class EventController {
 
     private final EventMapper eventMapper;
