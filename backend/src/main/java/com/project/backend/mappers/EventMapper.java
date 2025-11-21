47029f782f0e3220f2f12e71249e736995c95516
Aditya Tile <adityatile99@gmail.com>
2025-11-21 11:08:05 +0530

    feat: create DTOs for listing events and their ticket types

--- backend/src/main/java/com/project/backend/mappers/EventMapper.java
+++ backend/src/main/java/com/project/backend/mappers/EventMapper.java
@@ -2,10 +2,9 @@ package com.project.backend.mappers;
 
 import com.project.backend.domain.CreateEventRequest;
 import com.project.backend.domain.CreateTicketTypeRequest;
-import com.project.backend.domain.dtos.CreateEventRequestDto;
-import com.project.backend.domain.dtos.CreateEventResponseDto;
-import com.project.backend.domain.dtos.CreateTicketTypeResponseDto;
+import com.project.backend.domain.dtos.*;
 import com.project.backend.domain.entities.Event;
+import com.project.backend.domain.entities.TicketType;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
@@ -17,5 +16,9 @@ public interface EventMapper {
     CreateEventRequest fromDto(CreateEventRequestDto dto);
 
     CreateEventResponseDto toDto(Event event);
+
+    ListEventTicketTypeResponseDto toDto(TicketType ticketType);
+
+    ListEventResponseDto toListEventResponseDto(Event event);
 
 }
